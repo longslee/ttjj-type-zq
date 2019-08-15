b321da503d03f8d70e53004616f50d0cb6cf53e2
longslee <penglongjia@gmail.com>
2019-08-16 00:35:31 +0800

    余弦相似性

--- src/main/java/com/lee/util/MathUtil.java
+++ src/main/java/com/lee/util/MathUtil.java
@@ -2,6 +2,7 @@ package com.lee.util;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class MathUtil {
     private static final Map<String,Double> perfect = new HashMap<String,Double>();
@@ -13,5 +14,22 @@ public class MathUtil {
         //perfect.put("managerMaxEarning",);  //单位 w
         perfect.put("managerDurationExp",100.0); // 经验值 图
         perfect.put("managerEarnRation",100.0); //收益率，也是从图
+    }
+
+    /**
+     * 余弦相似度
+     * @param vector_1 向量1
+     * @param vector_2 向量2
+     * @return -1 截然相反 exactly opposite；1 完全相同 exactly the same；0 不相关 indicating orthogonality or decorrelation
+     */
+    public static double cosSimilarity(Map<String,Double> vector_1,Map<String,Double> vector_2){
+
+        double scalar = 0.0,norm_1 = 0.0,norm_2 = 0.0;
+        Set<String> v1Keys = vector_1.keySet();
+        Set<String> v2Keys = vector_2.keySet();
+
+        //下一步求交集
+
+        return 2.0;
     }
 }
